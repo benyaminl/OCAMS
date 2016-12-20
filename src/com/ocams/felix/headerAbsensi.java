1db7f3dac9cc7edb8838d07d555203a074a1d3d5
Gp62 <Gp62@MSI>
2016-12-20 17:32:17 +0700

    db and code exploit

--- src/com/ocams/felix/headerAbsensi.java
+++ src/com/ocams/felix/headerAbsensi.java
@@ -5,17 +5,37 @@
  */
 package com.ocams.felix;
 
+import java.util.ArrayList;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Gp62
  */
 public class headerAbsensi extends javax.swing.JFrame {
+DefaultTableModel tm;
+ArrayList<String[]> gviewData;
+        DefaultComboBoxModel<String> cbtanggal;
 
     /**
      * Creates new form headerAbsensi
      */
     public headerAbsensi() {
         initComponents();
+        String header[] = {"ID Pegawai ","Time IN","Time Out","Tanggal"};
+        tm = new DefaultTableModel(null,header);
+      gviewData = OCAMS.SQL.executeQueryGetArray("Select * from absensi");
+               // String bulan[] = {"Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"};
+//cbtanggal = new DefaultComboBoxModel(bulan); ** Ganti ke DTP untuk search nanti lempar isinya ke search
+
+//jComboBox1.setModel(cbtanggal);
+
+        jTable1.setModel(tm);
+        for(String[] d: gviewData){
+            tm.addRow(d);
+        }
     }
 
     /**
@@ -32,15 +52,15 @@ public class headerAbsensi extends javax.swing.JFrame {
         jTable1 = new javax.swing.JTable();
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
-        jRadioButton3 = new javax.swing.JRadioButton();
+        rbID = new javax.swing.JRadioButton();
+        rbTgl = new javax.swing.JRadioButton();
+        rbIDTgl = new javax.swing.JRadioButton();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jComboBox1 = new javax.swing.JComboBox<>();
         jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -57,27 +77,27 @@ public class headerAbsensi extends javax.swing.JFrame {
 
         jLabel1.setText("Search   :");
 
-        buttonGroup1.add(jRadioButton1);
-        jRadioButton1.setText("ID");
-        jRadioButton1.addActionListener(new java.awt.event.ActionListener() {
+        buttonGroup1.add(rbID);
+        rbID.setText("ID");
+        rbID.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jRadioButton1ActionPerformed(evt);
+                rbIDActionPerformed(evt);
             }
         });
 
-        buttonGroup1.add(jRadioButton2);
-        jRadioButton2.setText("Tanggal");
-        jRadioButton2.addActionListener(new java.awt.event.ActionListener() {
+        buttonGroup1.add(rbTgl);
+        rbTgl.setText("Tanggal");
+        rbTgl.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jRadioButton2ActionPerformed(evt);
+                rbTglActionPerformed(evt);
             }
         });
 
-        buttonGroup1.add(jRadioButton3);
-        jRadioButton3.setText("ID & Tanggal");
-        jRadioButton3.addActionListener(new java.awt.event.ActionListener() {
+        buttonGroup1.add(rbIDTgl);
+        rbIDTgl.setText("ID & Tanggal");
+        rbIDTgl.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jRadioButton3ActionPerformed(evt);
+                rbIDTglActionPerformed(evt);
             }
         });
 
@@ -88,6 +108,11 @@ public class headerAbsensi extends javax.swing.JFrame {
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         jButton1.setText("Search");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -100,11 +125,11 @@ public class headerAbsensi extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel1)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jRadioButton1)
+                        .addComponent(rbID)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jRadioButton2)
+                        .addComponent(rbTgl)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jRadioButton3)
+                        .addComponent(rbIDTgl)
                         .addGap(45, 45, 45)
                         .addComponent(jLabel2)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -124,9 +149,9 @@ public class headerAbsensi extends javax.swing.JFrame {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
-                    .addComponent(jRadioButton1)
-                    .addComponent(jRadioButton2)
-                    .addComponent(jRadioButton3)
+                    .addComponent(rbID)
+                    .addComponent(rbTgl)
+                    .addComponent(rbIDTgl)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2)
                     .addComponent(jLabel3)
@@ -139,17 +164,41 @@ public class headerAbsensi extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jRadioButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton1ActionPerformed
+    private void rbIDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbIDActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jRadioButton1ActionPerformed
+    }//GEN-LAST:event_rbIDActionPerformed
 
-    private void jRadioButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton2ActionPerformed
+    private void rbTglActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbTglActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jRadioButton2ActionPerformed
+    }//GEN-LAST:event_rbTglActionPerformed
 
-    private void jRadioButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jRadioButton3ActionPerformed
+    private void rbIDTglActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rbIDTglActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jRadioButton3ActionPerformed
+    }//GEN-LAST:event_rbIDTglActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        if(rbID.isSelected()){
+            gviewData = OCAMS.SQL.executeQueryGetArray("Select * from absensi where ID_Pegawai LIKE '%" + jTextField1.getText() + "%'");
+        }
+        
+        if(rbTgl.isSelected()){
+           // gviewData = OCAMS.SQL.executeQueryGetArray("SELECT * FROM `absensi` WHERE `Tanggal` <= '" + jTextField1.getText() + "'");
+           //Fitur search berdasarkan tanggal nanti
+        }
+        
+        if(rbIDTgl.isSelected()){            
+            gviewData = OCAMS.SQL.executeQueryGetArray("Select * from absensi where ID_Pegawai LIKE '" + jTextField1.getText() + "' and Tanggal '"); //DTP Value here
+        }
+        
+        for (int i = 0; i < tm.getRowCount(); i++) {
+            tm.removeRow(i);
+        }
+        
+        for(String[] d: gviewData){
+            tm.addRow(d);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -193,11 +242,11 @@ public class headerAbsensi extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
-    private javax.swing.JRadioButton jRadioButton3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JRadioButton rbID;
+    private javax.swing.JRadioButton rbIDTgl;
+    private javax.swing.JRadioButton rbTgl;
     // End of variables declaration//GEN-END:variables
 }
